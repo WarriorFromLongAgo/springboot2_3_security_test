986747946b5bc421029acf7ec09690d612aac87c
fff <651267797@qq.com>
2020-08-03 00:51:35 +0800

    1

---
+++ src/main/java/com/xuegao/springboot2_3_security_test/Springboot23SecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.springboot2_3_security_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot23SecurityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot23SecurityTestApplication.class, args);
+    }
+
+}
