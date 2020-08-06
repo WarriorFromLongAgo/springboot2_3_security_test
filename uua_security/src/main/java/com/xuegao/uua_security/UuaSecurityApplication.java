cd00b1485d1829d0c672786a58cedb36534da34e
fff <651267797@qq.com>
2020-08-06 23:49:27 +0800

    1

---
+++ uua_security/src/main/java/com/xuegao/uua_security/UuaSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.uua_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuaSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UuaSecurityApplication.class, args);
+    }
+
+}
---
+++ uua_security/src/test/java/com/xuegao/uua_security/UuaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.uua_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UuaSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
